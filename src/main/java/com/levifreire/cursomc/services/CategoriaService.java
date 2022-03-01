09b8650a64bef2bc3533df96cb4ab5dcda76aa4e
Levi Freire <levifreire7@gmail.com>
2022-02-28 21:19:47 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/levifreire/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.levifreire.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.levifreire.cursomc.domain.Categoria;
+import com.levifreire.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
