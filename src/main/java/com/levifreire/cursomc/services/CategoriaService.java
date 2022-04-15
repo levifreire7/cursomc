d9a1c599a2b929623e8dd2a2a22a3898c9dd35c8
Levi Freire <levifreire7@gmail.com>
2022-04-14 21:48:19 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/levifreire/cursomc/services/CategoriaService.java
+++ src/main/java/com/levifreire/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.levifreire.cursomc.domain.Categoria;
+import com.levifreire.cursomc.dto.CategoriaDTO;
 import com.levifreire.cursomc.repositories.CategoriaRepository;
 import com.levifreire.cursomc.services.exceptions.DataIntegrityException;
 import com.levifreire.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
