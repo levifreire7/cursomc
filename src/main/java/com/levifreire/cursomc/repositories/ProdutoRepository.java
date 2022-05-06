22e26539f0ac604987b91cac717ccd1b51e2d46b
Levi Freire <levifreire7@gmail.com>
2022-05-06 16:49:42 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/levifreire/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/levifreire/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.levifreire.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.levifreire.cursomc.domain.Categoria;
 import com.levifreire.cursomc.domain.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,
+			Pageable pageRequest);
 
 }
--- src/main/java/com/levifreire/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/levifreire/cursomc/resources/CategoriaResource.java
@@ -62,19 +62,16 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page, 
-			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
-			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
-			@RequestParam(value = "direction", defaultValue = "ASC")String direction){
-		
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
 		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	
 
 }
