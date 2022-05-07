363d8be0b0de59fffd318d24e79730114e39a609
Levi Freire <levifreire7@gmail.com>
2022-05-06 22:03:40 -0300

    Inserindo Pedido

--- src/main/java/com/levifreire/cursomc/domain/ItemPedido.java
+++ src/main/java/com/levifreire/cursomc/domain/ItemPedido.java
@@ -37,6 +37,10 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
@@ -44,6 +48,10 @@ public class ItemPedido implements Serializable {
 
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 	public ItemPedidoPK getId() {
