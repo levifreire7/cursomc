d1f0f549db8d28a45219feced19f16616bdd4f66
Levi Freire <levifreire7@gmail.com>
2022-07-14 15:53:51 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/levifreire/cursomc/services/DBService.java
+++ src/main/java/com/levifreire/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.levifreire.cursomc.domain.PagamentoComCartao;
 import com.levifreire.cursomc.domain.Pedido;
 import com.levifreire.cursomc.domain.Produto;
 import com.levifreire.cursomc.domain.enums.EstadoPagamento;
+import com.levifreire.cursomc.domain.enums.Perfil;
 import com.levifreire.cursomc.domain.enums.TipoCliente;
 import com.levifreire.cursomc.repositories.CategoriaRepository;
 import com.levifreire.cursomc.repositories.CidadeRepository;
@@ -115,13 +116,19 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "fic21212121@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("999999999", "888888888"));
 
+		Cliente cli2 = new Cliente(null, "Ana Costa", "levifreire7@gmail.com", "53305449047", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("954999999", "748888888"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2018", "Sala 800", "Centro", "38777012", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli1.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
